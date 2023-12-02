af82712f98728ca2cae87246a2b80166321bff30
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-01 23:55:32 -0500

    Redesigned MySet list and MySet detail pages to be more intuitive

--- frontend/src/components/MySetDetailComponent.vue
+++ frontend/src/components/MySetDetailComponent.vue
@@ -1,70 +1,72 @@
 <template>
     <div class="set-detail-container">
         <div v-if="error" class="error-message">{{ error }}</div>
-        <div class="set-banner">
+        
+        <!-- Editable Study Set Details -->
+        <div class="set-banner" v-if="editingSet">
+            <input type="text" v-model="setDetail.title" />
+            <textarea v-model="setDetail.description"></textarea>
+            <button @click="updateSetDetails">✔</button>
+            <button @click="confirmDeleteSet">Delete Set</button>
+        </div>
+        <div class="set-banner" v-else>
             <h1>{{ setDetail.title }}</h1>
             <p>{{ setDetail.description || 'No description provided.' }}</p>
+            <button @click="editSetDetails">✏️</button>
         </div>
 
+        <!-- Study Terms List -->
         <div class="study-terms">
             <h2>Study Terms</h2>
-            <button class="create-button" @click="createNewTerm">Create New Term</button>
+            <!-- Inline form to create a new term -->
+            <div class="term-item new-term-form" v-if="creatingNewTerm">
+                <input type="text" placeholder="Front text" v-model="newTerm.front_text" />
+                <input type="text" placeholder="Back text" v-model="newTerm.back_text" />
+                <button @click="createTerm">Create</button>
+            </div>
+
+            <button @click="toggleCreateNewTerm">Create New Term</button>
+
             <div v-for="term in studyTerms" :key="term.id" class="term-item">
-                <div>
-                    <strong>Front:</strong> {{ term.front_text }}
+                <div v-if="editingTerm && editingTerm.id === term.id">
+                    <!-- Edit form for the term -->
+                    <input type="text" v-model="termForm.front_text" />
+                    <input type="text" v-model="termForm.back_text" />
+                    <button @click="updateTerm">✔️ Save</button>
+                    <button @click="confirmDeleteTerm(term.id)">Delete</button>
                 </div>
-                <div>
-                    <strong>Back:</strong> {{ term.back_text }}
+                <div v-else>
+                    <!-- Display term details -->
+                    <div><strong>Front:</strong> {{ term.front_text }}</div>
+                    <div><strong>Back:</strong> {{ term.back_text }}</div>
+                    <button @click="editTerm(term)">Edit</button>
+                    <!-- Delete button is not shown here -->
                 </div>
-                <button class="edit-button" @click="editTerm(term)">Edit</button>
-                <button class="delete-button" @click="confirmDeleteTerm(term.id)">Delete</button>
-                <button class="duplicate-button" @click="duplicateTerm(term)">Duplicate</button>
             </div>
         </div>
-
-        <ModalComponent :showModal="showModal" @update:showModal="showModal = $event">
-            <form @submit.prevent="submitForm">
-                <div v-if="modalError" class="error-message">{{ modalError }}</div>
-                <label for="front_text">Front Text:</label>
-                <input type="text" id="front_text" v-model="termForm.front_text" required>
-                
-                <label for="back_text">Back Text:</label>
-                <input type="text" id="back_text" v-model="termForm.back_text" required>
-                
-                <!-- Include other inputs for additional term attributes here -->
-
-                <div class="modal-footer">
-                    <button type="button" @click="toggleModal">Cancel</button>
-                    <button type="submit">{{ isEditMode ? 'Update Term' : 'Create Term' }}</button>
-                </div>
-            </form>
-        </ModalComponent>
     </div>
 </template>
 
 <script>
-import ModalComponent from './ModalComponent.vue'; // Adjust path as necessary
 import { axiosAuthInstance } from '../utils/axios-config';
 
 export default {
-    components: {
-        ModalComponent
-    },
     data() {
         return {
             setDetail: {},
             studyTerms: [],
             error: null,
-            modalError: null,
-            showModal: false,
-            isEditMode: false,
+            editingSet: false,
+            editingTerm: null,
             editingTermId: null,
+            showModal: false,
             termForm: {
-                study_set: null,
                 front_text: '',
                 back_text: '',
-                // Include other term attributes here
-            }
+                study_set: null,
+            },
+            creatingNewTerm: false,
+            newTerm: { front_text: '', back_text: '', study_set: null },
         };
     },
     created() {
@@ -74,105 +76,140 @@ export default {
         async fetchSetDetail() {
             try {
                 const setId = this.$route.params.id;
-                this.termForm.study_set = setId;
+                this.termForm.study_set = setId; // Linking the term to the set
                 const setResponse = await axiosAuthInstance.get(`study_sets/study_sets/${setId}/`);
                 this.setDetail = setResponse.data;
+
                 const termsResponse = await axiosAuthInstance.get(`study_sets/terms_in_set/${setId}/`);
                 this.studyTerms = termsResponse.data;
             } catch (error) {
-                // If set not found (no permission)
-                if (error.response && error.response.data.detail === "Not found.") {
-                    this.$router.push({ path: '/my-study-sets/' })
-                } else {
-                    this.error = error.response ? error.response.data.detail : 'An error occurred';
-                }
+                this.error = error.response ? error.response.data.detail : 'An error occurred';
             }
         },
-        editTerm(term) {
-            this.isEditMode = true;
-            this.editingTermId = term.id;
-            this.termForm = {
-                study_set: this.termForm.study_set,
-                ...term
-            };
+
+        editSetDetails() {
+            this.editingSet = true;
+        },
+
+        async updateSetDetails() {
+            try {
+                await axiosAuthInstance.put(`/study_sets/study_sets/${this.setDetail.id}/`, this.setDetail);
+                this.editingSet = false;
+                // Refresh set details
+                this.fetchSetDetail();
+            } catch (error) {
+                this.error = error.response ? error.response.data.detail : 'Error updating set details';
+            }
+        },
+
+        confirmDeleteSet() {
+            if (confirm("Are you sure you want to delete this set?")) {
+                this.deleteSet();
+            }
+        },
+        async deleteSet() {
+            try {
+                await axiosAuthInstance.delete(`study_sets/study_sets/${this.setDetail.id}/`);
+                this.$router.push('/my-study-sets/'); // Redirect after deletion
+            } catch (error) {
+                this.error = error.response ? error.response.data.detail : 'Error deleting set';
+            }
+        },
+
+        toggleCreateNewTerm() {
+            this.creatingNewTerm = !this.creatingNewTerm;
+            this.resetNewTermForm();
+            if (this.creatingNewTerm) {
+                this.newTerm.study_set = this.setDetail.id;
+            }
+        },
+        resetNewTermForm() {
+            this.newTerm = { front_text: '', back_text: '', study_set: this.setDetail.id };
+        },
+        createTerm() {
+            axiosAuthInstance.post('/study_sets/study_terms/', this.newTerm)
+                .then(() => {
+                    this.creatingNewTerm = false;
+                    this.fetchSetDetail();
+                })
+                .catch(error => {
+                    this.error = error.response ? error.response.data.detail : 'Error creating term';
+                });
+        },
+
+        createNewTerm() {
+            this.editingTermId = null;
+            this.termForm = { front_text: '', back_text: '', study_set: this.setDetail.id };
             this.showModal = true;
         },
+
+        editTerm(term) {
+            // Check if we are already in editing mode for this term
+            if (this.editingTerm && this.editingTerm.id === term.id) {
+                // If we are, turn off editing mode
+                this.editingTerm = null;
+            } else {
+                // Otherwise, enter editing mode for this term
+                this.editingTerm = term;
+                this.termForm = { ...term };
+            }
+        },
+
+        async updateTerm() {
+            try {
+                if (this.editingTerm) {
+                    await axiosAuthInstance.put(`/study_sets/study_terms/${this.editingTerm.id}/`, this.termForm);
+                    // Turn off editing mode
+                    this.editingTerm = null;
+                    this.fetchSetDetail();
+                }
+            } catch (error) {
+                this.error = error.response ? error.response.data.detail : "An error occurred while updating the term.";
+            }
+        },
+
+        async submitTermForm() {
+            try {
+                if (this.editingTermId) {
+                    await axiosAuthInstance.put(`/study_sets/study_terms/${this.editingTermId}/`, this.termForm);
+                } else {
+                    await axiosAuthInstance.post(`/study_sets/study_terms/`, this.termForm);
+                }
+                this.showModal = false;
+                this.fetchSetDetail();
+            } catch (error) {
+                this.error = error.response ? error.response.data.detail : "An error occurred while processing the term.";
+            }
+        },
+
         async confirmDeleteTerm(termId) {
             if (confirm("Are you sure you want to delete this term?")) {
                 await this.deleteTerm(termId);
             }
         },
+
         async deleteTerm(termId) {
             try {
                 await axiosAuthInstance.delete(`study_sets/study_terms/${termId}/`);
-                this.fetchSetDetail(); // Refresh the list after deletion
+                this.fetchSetDetail();
             } catch (error) {
                 this.error = error.response ? error.response.data.detail : 'An error occurred while deleting the term';
             }
         },
+
         async duplicateTerm(term) {
-            const newTerm = {
-                // Copy the fields you need from the term to duplicate
-                study_set: this.termForm.study_set,
-                front_text: term.front_text,
-                back_text: term.back_text,
-                // Add other fields if necessary
-            };
+            const newTerm = { ...term, id: undefined, study_set: this.termForm.study_set };
             try {
                 await axiosAuthInstance.post(`study_sets/study_terms/`, newTerm);
-                this.fetchSetDetail(); // Refresh the list after duplication
+                this.fetchSetDetail();
             } catch (error) {
                 this.error = error.response ? error.response.data.detail : 'An error occurred while duplicating the term';
             }
         },
-        createNewTerm() {
-            this.isEditMode = false;
-            this.editingTermId = null;
-            this.termForm = {
-                study_set: this.termForm.study_set,
-                front_text: '',
-                back_text: '',
-                // Reset other term attributes
-            };
-            this.showModal = true;
-        },
-        async submitForm() {
-            this.modalError = null;
-            if (this.termForm.front_text == '' || this.termForm.back_text == '') {
-                this.modalError = "Please fill in both sides of the term!";
-                return;
-            }
-            try {
-                if (this.isEditMode) {
-                    await axiosAuthInstance.put(`study_sets/study_terms/${this.editingTermId}/`, this.termForm);
-                } else {
-                    await axiosAuthInstance.post(`study_sets/study_terms/`, this.termForm);
-                }
-                this.toggleModal();
-                this.fetchSetDetail();
-            } catch (error) {
-                this.modalError = error.response ? error.response.data.detail : "An error occurred while processing the term.";
-            }
-        },
-        toggleModal() {
-            this.showModal = !this.showModal;
-            if (!this.showModal) {
-                this.resetForm();
-            }
-        },
-        resetForm() {
-            this.isEditMode = false;
-            this.editingTermId = null;
-            this.termForm = {
-                study_set: this.termForm.study_set,
-                front_text: '',
-                back_text: '',
-                // Reset other term attributes
-            };
-        },
     }
 };
 </script>
+
 
 <style scoped>
 .set-detail-container {
--- frontend/src/components/MySetsComponent.vue
+++ frontend/src/components/MySetsComponent.vue
@@ -2,150 +2,130 @@
     <div class="set-container">
         <h1>My Study Sets</h1>
         <div v-if="error" class="error-message">{{ error }}</div>
-        <button @click="toggleModal">Create New Set</button>
-        <div class="sets-list" v-if="public_sets">
+
+        <!-- Create New Set - Inline Form -->
+        <div class="set-item new-set-form" v-if="creatingNewSet">
+            <input type="text" placeholder="Set Title" v-model="newSet.title" />
+            <textarea placeholder="Set Description" v-model="newSet.description"></textarea>
+            <button @click="createSet">Create</button>
+        </div>
+
+        <button @click="toggleCreateNewSet">Create New Set</button>
+
+        <!-- Study Sets List -->
+        <div class="sets-list">
             <div class="set-item" v-for="set in public_sets" :key="set.id">
-                <router-link class="info-panel" :to="`/my-study-set/${set.id}`">
-                    <h3>{{ set.title }}</h3>
-                    <p>{{ set.description || "No description provided." }}</p>
-                </router-link>
-                <button class="edit-button" @click="prepareEditSet(set)">Edit</button>
-                <button class="delete-button" @click="confirmDelete(set.id)">Delete</button>
+                <div v-if="editingSetId === set.id">
+                    <input type="text" v-model="set.title" />
+                    <textarea v-model="set.description"></textarea>
+                    <select v-model="set.private">
+                        <option value="false">Public</option>
+                        <option value="true">Private</option>
+                    </select>
+                    <button @click="updateSet(set)">✔</button>
+                    <button @click="deleteSet(set.id)">Delete</button>
+                </div>
+                <div v-else>
+                    <router-link :to="`/my-study-set/${set.id}`">{{ set.title }}</router-link>
+                    <p>{{ set.description }}</p>
+                    <button @click="editSet(set)">✏️</button>
+                </div>
             </div>
         </div>
+
+        <!-- Pagination -->
         <div class="pagination">
             <button @click="navigatePage('previous')" :disabled="!pagination_links.previous">Previous</button>
             <span>Page {{ current_page }} of {{ total_pages }}</span>
             <button @click="navigatePage('next')" :disabled="!pagination_links.next">Next</button>
         </div>
-        <ModalComponent :showModal="showModal" @update:showModal="showModal = $event">
-            <form @submit.prevent="submitForm">
-                <div v-if="modal_error" class="error-message">{{ modal_error }}</div>
-                <label for="title">Title:</label>
-                <input type="text" id="title" v-model="study_set_form.title" required>
-                <label for="description">Description:</label>
-                <textarea id="description" v-model="study_set_form.description"></textarea>
-                <label for="private">Private:</label>
-                <input type="checkbox" id="private" v-model="study_set_form.private">
-                <div class="modal-footer">
-                    <button type="button" @click="toggleModal">Cancel</button>
-                    <button type="submit">{{ isEditMode ? 'Update Set' : 'Create Set' }}</button>
-                </div>
-            </form>
-        </ModalComponent>
     </div>
 </template>
 
 <script>
-import ModalComponent from './ModalComponent.vue'; // Adjust path as necessary
 import { axiosAuthInstance } from '../utils/axios-config';
 
 export default {
-    components: {
-        ModalComponent
-    },
     data() {
         return {
             error: null,
-            public_sets: null,
+            public_sets: [],
             pagination_links: {},
             current_page: 1,
             total_pages: 1,
-            showModal: false,
-            isEditMode: false,
+            creatingNewSet: false,
             editingSetId: null,
-            modal_error: null,
-            study_set_form: {
-                title: '',
-                description: '',
-                private: false,
-            }
+            newSet: { title: '', description: '', private: false },
         };
     },
     methods: {
-        toggleModal() {
-            this.showModal = !this.showModal;
-            if (!this.showModal) {
-                this.resetForm();
+        toggleCreateNewSet() {
+            this.creatingNewSet = !this.creatingNewSet;
+            if (!this.creatingNewSet) {
+                this.resetNewSetForm();
             }
         },
-        resetForm() {
-            this.isEditMode = false;
-            this.editingSetId = null;
-            this.study_set_form = { title: '', description: '', private: false };
+        resetNewSetForm() {
+            this.newSet = { title: '', description: '', private: false };
         },
-        prepareEditSet(set) {
-            this.isEditMode = true;
+        createSet() {
+            // API call to create a new set
+            axiosAuthInstance.post('/study_sets/study_sets/', this.newSet)
+                .then(response => {
+                    this.public_sets.push(response.data);
+                    this.toggleCreateNewSet();
+                })
+                .catch(error => {
+                    this.error = error.response ? error.response.data.message : 'Error creating set';
+                });
+        },
+        editSet(set) {
             this.editingSetId = set.id;
-            this.study_set_form = { ...set };
-            this.showModal = true;
         },
-        async submitForm() {
-            this.modal_error = null;
-            if (this.study_set_form.title == '') {
-                this.modal_error = "Please enter a title for your Study Set!";
-                return;
-            }
-            try {
-                if (this.isEditMode) {
-                    await axiosAuthInstance.put(`study_sets/study_sets/${this.editingSetId}/`, this.study_set_form);
-                } else {
-                    await axiosAuthInstance.post('study_sets/study_sets/', this.study_set_form);
-                }
-                this.toggleModal();
-                this.getMyStudySets(this.current_page);
-            } catch (error) {
-                this.modal_error = error.response ? error.response.data.detail : "An error occurred while processing the set.";
-            }
+        updateSet(set) {
+            // API call to update the set
+            axiosAuthInstance.put(`/study_sets/study_sets/${set.id}/`, set)
+                .then(() => {
+                    this.editingSetId = null;
+                    // Optionally refresh the list of sets
+                })
+                .catch(error => {
+                    this.error = error.response ? error.response.data.message : 'Error updating set';
+                });
         },
-        confirmDelete(setId) {
-            if (confirm("Are you sure you want to delete this set?")) {
-                this.deleteStudySet(setId);
-            }
+        deleteSet(setId) {
+            // API call to delete the set
+            axiosAuthInstance.delete(`/study_sets/study_sets/${setId}/`)
+                .then(() => {
+                    this.public_sets = this.public_sets.filter(set => set.id !== setId);
+                })
+                .catch(error => {
+                    this.error = error.response ? error.response.data.message : 'Error deleting set';
+                });
         },
-        async deleteStudySet(setId) {
-            try {
-                await axiosAuthInstance.delete(`study_sets/study_sets/${setId}/`);
-                this.getMyStudySets(this.current_page); // Refresh the list after deletion
-            } catch (error) {
-                this.error = error.response ? error.response.data.detail : 'An error occurred while deleting the set';
-            }
-        },
-        async getMyStudySets(page) {
-            this.error = null;
-            try {
-                const response = await axiosAuthInstance.get(`study_sets/my_sets/?page=${page}`);
-                this.public_sets = response.data.results;
-                this.pagination_links = response.data.links;
-                this.current_page = response.data.current_page;
-                this.total_pages = response.data.total_pages;
-            } catch (error) {
-                this.error = error.response ? error.response.data.detail : 'An error occurred';
-            }
-        },
-        navigatePage(direction) {
+            navigatePage(direction) {
             const nextPage = direction === 'next' ? this.current_page + 1 : this.current_page - 1;
-            this.$router.push({ path: '/my-study-sets/' + nextPage.toString() });
-        }
-    },
-    watch: {
-        '$route.params.page': {
-            immediate: true,
-            handler(newPage) {
-                this.getMyStudySets(newPage || 1);
-            }
-        }
+            this.getMyStudySets(nextPage);
+        },
+        getMyStudySets(page) {
+            axiosAuthInstance.get(`/study_sets/my_sets/?page=${page}`)
+                .then(response => {
+                    this.public_sets = response.data.results;
+                    this.pagination_links = response.data.links;
+                    this.current_page = response.data.current_page;
+                    this.total_pages = response.data.total_pages;
+                })
+                .catch(error => {
+                    this.error = error.response ? error.response.data.message : 'Error fetching sets';
+                });
+        },
     },
     mounted() {
-        const initialPage = this.$route.params.page || 1;
-        this.getMyStudySets(initialPage);
+        // Fetch initial data
+        this.getMyStudySets(1);
     }
-}
+};
 </script>
-
-<!-- <style scoped>
-/* @import "@/assets/container-list-style.css"; */
-</style> -->
 
 <style scoped>
 .set-container {
@@ -154,68 +134,38 @@ export default {
     padding: 20px;
 }
 
-.sets-list {
+.sets-list, .new-set-form {
     margin-top: 20px;
 }
 
 .set-item {
-    display: flex;
-    align-items: center;
-    justify-content: right;
     background-color: #f8f8f8;
     padding: 15px;
     margin-bottom: 10px;
     border-radius: 5px;
 }
 
-.set-item a {
-    display: inline-block;
-    flex-grow: 1;
-    margin-left: 1rem;
-    text-align: left;
+.set-item input, .set-item textarea {
+    width: 100%;
+    margin-bottom: 10px;
 }
 
-.set-item h3 {
-    display: inline-block;
-    width: 50%;
-    font-size: 1.2em;
-}
-
-.set-item p {
-    display: inline-block;
-    width: 50%;
-    color: #666;
-    font-size: 0.9em;
+.set-item select {
+    margin-bottom: 10px;
 }
 
 .set-item button {
-    margin-left: 10px;
     padding: 5px 10px;
-    font-size: 0.9em;
+    margin-right: 5px;
     border: none;
     border-radius: 5px;
     cursor: pointer;
 }
 
-.set-item button:hover {
-    opacity: 0.9;
-}
-
-.set-item .edit-button {
-    display: inline-block;
-    width: 8rem;
-    max-width: 8rem;
-    height: 3rem;
-    background-color: #4CAF50;
-    color: white;
-}
-
-.set-item .delete-button {
-    width: 5rem;
-    max-width: 5rem;
-    height: 3rem;
-    background-color: #f44336;
-    color: white;
+.error-message {
+    color: #f44336;
+    text-align: center;
+    margin-top: 10px;
 }
 
 .pagination {
@@ -239,11 +189,5 @@ export default {
 .pagination button:disabled {
     background-color: #ccc;
     cursor: not-allowed;
-}
-
-.error-message {
-    color: #f44336;
-    text-align: center;
-    margin-top: 10px;
 }
 </style>
