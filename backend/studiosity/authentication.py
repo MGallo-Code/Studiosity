1a5c8c5539a973a9ca5835e5091103b7833e544e
MGallo-Code <mgallo.code@gmail.com>
2024-01-09 18:22:24 +0000

    Added comments to authentication methods/classes.

--- backend/studiosity/authentication.py
+++ backend/studiosity/authentication.py
@@ -5,8 +5,25 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom view to handle JWT token obtainment through HTTP-only cookies.
+
+    This view overrides the standard token obtainment behavior of TokenObtainPairView
+    to set the access and refresh tokens in HTTP-only cookies, enhancing the security
+    by not exposing tokens to client-side JavaScript.
+    """
+
     def post(self, request, *args, **kwargs):
+        """
+        Custom view to handle JWT token obtainment through HTTP-only cookies.
+
+        This view overrides the standard token obtainment behavior of TokenObtainPairView
+        to set the access and refresh tokens in HTTP-only cookies, enhancing the security
+        by not exposing tokens to client-side JavaScript.
+        """
+        # Validate login request by calling parent class's post method
         response = super().post(request, *args, **kwargs)
+        # If the login is successful, set cookies with access and refresh tokens
         if response.status_code == 200:
             access_token = response.data['access']
             refresh_token = response.data['refresh']
@@ -28,16 +45,38 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 
 class CustomTokenRefreshView(TokenRefreshView):
+    """
+    Custom view for refreshing JWT tokens, using the refresh token from HTTP-only cookies.
+
+    This view extends the standard TokenRefreshView behavior to retrieve the refresh token
+    from an HTTP-only cookie instead of expecting it in the request body or headers.
+    """
+
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST request to refresh JWT access token using a refresh token from cookies.
+
+        Args:
+            request: HttpRequest object.
+
+        Returns:
+            HttpResponse object with the new access token set in cookies if the refresh is successful.
+            Raises ValidationError if the refresh token is not provided or invalid.
+        """
+        # Validate refresh token from cookies
         refresh_token = request.COOKIES.get('studiosity_refresh_token')
         if not refresh_token:
             raise ValidationError('No refresh token provided')
         
+        # Necessary to update the request's data
         request.data._mutable = True
+        # Add the refresh token with the correct name for our super() call
         request.data.update({'refresh': refresh_token})
-
+        # Call super() to refresh our token,
+        #   now that it's been retrieved from cookies and properly set in the headers
         response = super().post(request, *args, **kwargs)
 
+        # If successful, set the new access token cookie.
         if response.status_code == 200:
             access_token = response.data['access']
             response.set_cookie(
@@ -51,6 +90,17 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 @api_view(['POST'])
 def logout_view(request):
+    """
+    API view to log out a user by deleting JWT tokens stored in HTTP-only cookies.
+
+    Deletes 'studiosity_access_token' and 'studiosity_refresh_token' cookies to log the user out.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        JsonResponse indicating successful logout.
+    """
     response = JsonResponse({ "detail": "Logged out successfully." })
     response.delete_cookie('studiosity_access_token')
     response.delete_cookie('studiosity_refresh_token')
--- backend/users/custom_authentication.py
+++ backend/users/custom_authentication.py
@@ -1,14 +1,31 @@
-# custom_authentication.py
-
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        # Get the token from the cookies. Assume it's stored under 'access_token'
+        """
+        Custom authentication method to authenticate users based on JWT token stored in HTTP cookies.
+
+        This method overrides the standard JWT authentication process to retrieve the JWT token
+        from a HTTP-only cookie, instead of the Authorization header. It then validates the token
+        to authenticate the user.
+
+        Args:
+            request: HttpRequest object containing metadata about the request.
+
+        Returns:
+            A tuple of (user, token) if authentication is successful, where 'user' is
+            the user associated with the token and 'token' is the validated JWT token.
+            Returns None if authentication fails, either due to missing token or validation failure.
+
+        Raises:
+            AuthenticationFailed: If the token is invalid, expired, or tampered.
+        """
+        # Get token from cookies (httponly)
         raw_token = request.COOKIES.get('studiosity_access_token')
+        # If cookies token not found, failed authentication.
         if raw_token is None:
             return None
-
+        # Validate token, return 
         validated_token = self.get_validated_token(raw_token)
-
         return self.get_user(validated_token), validated_token
