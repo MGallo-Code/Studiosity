4821976f510d867a8f03df2790e4419b9be456cf
MGallo-Code <mgallo.code@gmail.com>
2024-01-09 18:32:07 +0000

    Removed unnecessary admin.py files, edited comments in settings.py

--- backend/study_sets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import StudySet, StudyTerm, Tag
-
-admin.site.register(StudySet)
-admin.site.register(StudyTerm)
-admin.site.register(Tag)
--- backend/uploads/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import AudioFile, ImageFile
-
-
-admin.site.register(AudioFile)
-admin.site.register(ImageFile)
